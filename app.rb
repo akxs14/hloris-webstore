0445679773902790cd7da2bbad5f4505375e9613
angelos <angelos.kapsimanis@gmail.com>
2014-10-24 22:33:55 +0200

    Added english routes, default routes redirecting to en, added views

--- app.rb
+++ app.rb
@@ -7,18 +7,36 @@ configure do
   set :public_folder, 'public'
 end
 
+# Redirect to english pages for all default routes
 get '/' do
-  slim :index
+  redirect '/en'
 end
 
 get '/products' do
-  slim :products
+  redirect '/en/products'
 end
 
 get '/about_us' do
-  slim :about_us
+  redirect '/en/about_us'
 end
 
 get '/contact_us' do
+  redirect '/en/contact_us'
+end
+
+# English section
+get '/en' do
+  slim :index
+end
+
+get '/en/products' do
+  slim :products
+end
+
+get '/en/about_us' do
+  slim :about_us
+end
+
+get '/en/contact_us' do
   slim :contact_us
 end
